92425304764779902313888144f4d1e44818033c
nurmdrafi <nurmdrafi2014@gmail.com>
2022-04-02 22:43:10 +0600

    home content added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import './Home.css'
 
 const Home = () => {
-    return (
-        <div>
-            <h2>home</h2>
-        </div>
-    );
+  return (
+    <div className="home">
+      <h1>
+        Welcome to <b>Crypto <span>Watch</span></b>
+      </h1>
+      <Link to="/coins">
+        <button type="button" class="btn btn-dark">
+          Explore Coins
+        </button>
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
